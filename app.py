a1cb1add5ab9cc03ad5cb93b37a6b73d99707198
Gabe Fierro <gtfierro225@gmail.com>
2016-03-15 10:29:56 -0700

    multiple types of jobs

--- app.py
+++ app.py
@@ -30,6 +30,34 @@ def get_unique_filename(basename):
         tmp_name = os.path.join(app.config['UPLOAD_FOLDER'], str(i)+'_'+basename)
     return tmp_name
 
+def get_datafile_from_request(request):
+    data_file = request.files["datafile"]
+    if data_file and allowed_file(data_file.filename):
+        filename = secure_filename(data_file.filename)
+        filename = get_unique_filename(filename)
+        # save data file to local storage
+        data_file.save(filename)
+        return filename
+    return None
+
+@app.route('/mnv', methods=['POST'])
+def do_mnv():
+    assert request.method == 'POST'
+    datafile = get_datafile_from_request(request)
+    if datafile == None:
+        raise Exception("Bad datafile")
+    q.enqueue(run_mnv, datafile)
+    return
+
+@app.route('/mave', methods=['POST'])
+def do_mave():
+    assert request.method == 'POST'
+    datafile = get_datafile_from_request(request)
+    if datafile == None:
+        raise Exception("Bad datafile")
+    q.enqueue(run_mave, datafile)
+    return
+
 @app.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
